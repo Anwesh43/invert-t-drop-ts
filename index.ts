b3d9113b876b6531dd6c594a0f007132ea34d2b8
Anwesh43 <anweshthecool0@gmail.com>
2021-07-04 03:34:09 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -25,3 +25,37 @@ class ScaleUtil {
         return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawInvertTDrop(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sc1 : number = ScaleUtil.divideScale(scale, 0, parts)
+        const sc2 : number = ScaleUtil.divideScale(scale, 1, parts)
+        const sc3 : number = ScaleUtil.divideScale(scale, 2, parts)
+        const sc4 : number = ScaleUtil.divideScale(scale, 3, parts)
+        context.save()
+        context.translate(w / 2, h / 2 - (h / 2) * sc4)
+        context.rotate(rot * sc3)
+        context.save()
+        context.translate(0, (h / 2) * (1 - sc2))
+        DrawingUtil.drawLine(context, 0, 0, 0, size)
+        context.restore()
+        DrawingUtil.drawLine(context, -size / 2, 0, size / 2, 0)
+        context.restore()
+    }
+
+    static drawITDNode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = backColor 
+        DrawingUtil.drawInvertTDrop(context, scale)
+    }
+}
