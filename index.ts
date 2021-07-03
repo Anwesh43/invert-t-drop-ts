b48b070329108397d18dcf3e67470842db0304bb
Anwesh43 <anweshthecool0@gmail.com>
2021-07-04 03:27:03 +0530

    adding ScaleUtil class

--- index.ts
+++ index.ts
@@ -14,3 +14,14 @@ const backColor : string = "#BDBDBD"
 const rot : number = Math.PI 
 const w : number = window.innerWidth
 const h : number = window.innerHeight
+
+class ScaleUtil {
+
+    static maxScale(scale : number, i : number, n : number) : number {
+        return Math.max(0, scale - i / n)
+    }
+
+    static divideScale(scale : number, i : number, n : number) : number {
+        return Math.min(1 / n, ScaleUtil.divideScale(scale, i, n)) * n 
+    }
+}
