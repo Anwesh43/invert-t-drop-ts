1c09bd6ab58413fba73a08df735300b575e8d7a5
Anwesh43 <anweshthecool0@gmail.com>
2021-07-04 03:50:21 +0530

    adding InvertTDrop class

--- index.ts
+++ index.ts
@@ -176,3 +176,26 @@ class ITDNode {
         return this 
     }
 }
+
+class InvertTDrop {
+
+    curr : ITDNode = new ITDNode(0)
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
